6113c86a8c31556b8904eb7586b8e5ca0cf32bcb
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2021-10-17 01:28:56 +0200

    Add facebook webhock

--- src/Backend/mychat/mychat/settings.py
+++ src/Backend/mychat/mychat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*ztd$aj@)lvn)hvj^t=9&%#wpt%0+zr-&%rqtu%bhshe4sg=mf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    'mychat.base'
+
 ]
 
 MIDDLEWARE = [
--- src/Backend/mychat/mychat/urls.py
+++ src/Backend/mychat/mychat/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .base.api import FacebookWebhookView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", FacebookWebhookView.as_view(), name="angular-login"),
+
 ]
