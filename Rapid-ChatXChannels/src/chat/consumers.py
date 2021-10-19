a9d4ea5d317a9e72025fbed332c4f4758788b5c9
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2021-10-19 22:30:47 +0200

    Add ChatMessage

--- Rapid-ChatXChannels/src/chat/consumers.py
+++ Rapid-ChatXChannels/src/chat/consumers.py
@@ -5,6 +5,10 @@ from channels.generic.websocket import JsonWebsocketConsumer
 class ChatConsumer(JsonWebsocketConsumer):
 
     def connect(self) -> None:
+        await self.send({
+          "type":"websocket.send",
+          "text":"hello world"
+        })
         print('sssssssssssssssss')
 
     def disconnect(self, close_code) -> None:
