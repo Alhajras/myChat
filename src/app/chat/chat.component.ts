7294869d0a0e1e04abb3978a2158741a27a7520a
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2021-10-20 20:35:49 +0200

    Remove extra code

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -21,38 +21,12 @@ interface Option {
 })
 
 export class ChatComponent {
-  calendarValue: any
-  checkboxValue = ''
-  chipsValue = ''
-  cities: Option[]
-  contextMenuItems: MenuItem[]
-  countries: any[]
   customers: Customer[]
-  displayDialog = false
-  dropdownValue: any
-  events: any[]
-  filteredCountries: any[] = []
-  floatValue = ''
-  inputNumberValue = 0
-  listboxValue: any
-  menuItems: MenuItem[]
-  msgs: Message[]
   messages: ChatMessage[]
-  multiselectOptions: Option[]
-  multiselectValue: Option[] = []
-  products: Product[]
-  radioValue = ''
-  ratingValue = 0
-  selectButtonOptions: Option[]
-  selectButtonValue1: any
-  selectButtonValue2: any
-  selectedCountry: any
   selectedCustomers: Customer[] = []
-  selectedProduct: any
-  sliderValue = 0
-  switchValue = false
   tieredMenuItems: MenuItem[]
-  toggleValue = false
+  textArea: any;
+
   constructor (private readonly countryService: CountryService, private readonly messageService: MessageService,
     private readonly customerService: CustomerService, private readonly productService: ProductService, private readonly chatMessageServie: ChatMessageService,
     private readonly confirmationService: ConfirmationService) {
@@ -79,37 +53,7 @@ export class ChatComponent {
     //         websocket.onclose = function (e){
     //   console.log('closed', e)
     // }
-    this.countries = this.countryService.getCountries()
-    this.products = this.productService.getProductsSmall()
     this.customers = this.customerService.getCustomersLarge()
-
-    this.cities = [
-      { name: 'New York', code: 'NY' },
-      { name: 'Rome', code: 'RM' },
-      { name: 'London', code: 'LDN' },
-      { name: 'Istanbul', code: 'IST' },
-      { name: 'Paris', code: 'PRS' },
-    ]
-
-    this.multiselectOptions = [
-      { name: 'Australia', code: 'AU' },
-      { name: 'Brazil', code: 'BR' },
-      { name: 'China', code: 'CN' },
-      { name: 'Egypt', code: 'EG' },
-      { name: 'France', code: 'FR' },
-      { name: 'Germany', code: 'DE' },
-      { name: 'India', code: 'IN' },
-      { name: 'Japan', code: 'JP' },
-      { name: 'Spain', code: 'ES' },
-      { name: 'United States', code: 'US' },
-    ]
-
-    this.selectButtonOptions = [
-      { name: 'Option 1', code: 'O1' },
-      { name: 'Option 2', code: 'O2' },
-      { name: 'Option 3', code: 'O3' },
-    ]
-
     this.tieredMenuItems = [
       {
         label: 'Customers',
@@ -194,90 +138,9 @@ export class ChatComponent {
       },
     ]
 
-    this.menuItems = [
-      {
-        label: 'Customers',
-        items: [
-          {
-            label: 'New',
-            icon: 'pi pi-fw pi-plus',
-          },
-          {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-user-edit',
-          },
-        ],
-      },
-      {
-        label: 'Orders',
-        items: [
-          {
-            label: 'View',
-            icon: 'pi pi-fw pi-list',
-          },
-          {
-            label: 'Search',
-            icon: 'pi pi-fw pi-search',
-          },
 
-        ],
-      },
-    ]
-
-    this.contextMenuItems = [
-      {
-        label: 'Save',
-        icon: 'pi pi-save',
-      },
-      {
-        label: 'Update',
-        icon: 'pi pi-refresh',
-      },
-      {
-        label: 'Delete',
-        icon: 'pi pi-trash',
-      },
-      {
-        separator: true,
-      },
-      {
-        label: 'Options',
-        icon: 'pi pi-cog',
-      },
-    ]
-
-    this.msgs = [
-      { severity: 'success', summary: 'Success', detail: 'Message Content' },
-      { severity: 'info', summary: 'Info', detail: 'Message Content' },
-      { severity: 'warn', summary: 'Warning', detail: 'Message Content' },
-      { severity: 'error', summary: 'Error', detail: 'Message Content' },
-    ]
-
-    this.events = [
-      {
-        status: 'Ordered',
-        date: '15/10/2020 10:30',
-        icon: PrimeIcons.SHOPPING_CART,
-        color: '#9C27B0',
-        image: 'default.jpg',
-      },
-      { status: 'Processing', date: '15/10/2020 14:00', icon: PrimeIcons.COG, color: '#673AB7' },
-      { status: 'Shipped', date: '15/10/2020 16:15', icon: PrimeIcons.ENVELOPE, color: '#FF9800' },
-      { status: 'Delivered', date: '16/10/2020 10:00', icon: PrimeIcons.CHECK, color: '#607D8B' },
-    ]
   }
 
-  filterCountry ($event: any) {
-    const filtered: any[] = []
-    const query = 'aus'
-    for (const country of this.countries) {
-      if (country.name.toLowerCase().indexOf(query.toLowerCase()) === 0) {
-        filtered.push(country)
-      }
-    }
-
-    this.filteredCountries = filtered
-  }
 
   onRowSelect ($event: any, op: OverlayPanel) {
     // @ts-expect-error
@@ -285,13 +148,7 @@ export class ChatComponent {
     op.hide()
   }
 
-  openDialog () {
-    this.displayDialog = true
-  }
 
-  closeDialog () {
-    this.displayDialog = false
-  }
 
   showToast (severity: string) {
     this.messageService.add({ severity, summary: 'Message Summary', detail: 'Message Detail', life: 3000 })
