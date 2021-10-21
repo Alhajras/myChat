6b466789799b2334566dc1e047602d8bc8d96283
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2021-10-21 21:12:10 +0200

    Add conversation and chat messages

--- Rapid-ChatXChannels/src/chat/models.py
+++ Rapid-ChatXChannels/src/chat/models.py
@@ -8,6 +8,18 @@ class ChannelsTypes(models.TextChoices):
   INSTAGRAM = "Instagram"
 
 
+class ChatUser(models.Model):
+  first_name = models.CharField(max_length=25)
+  last_name = models.CharField(max_length=25)
+  email = models.EmailField()
+
+
+class Conversation(models.Model):
+  participant_1 = models.ForeignKey(ChatUser,on_delete=models.PROTECT, related_name='participant_1')
+  participant_2 = models.ForeignKey(ChatUser,on_delete=models.PROTECT, related_name='participant_2')
+  created_at = models.DateTimeField(auto_now_add=True)
+  deleted = models.BooleanField()
+
 class ChatMessage(models.Model):
   message = models.TextField()
   timestamp = models.DateTimeField(auto_now_add=True)
@@ -15,3 +27,9 @@ class ChatMessage(models.Model):
   channel = models.CharField(
     max_length=10, choices=ChannelsTypes.choices, default=ChannelsTypes.LOCAL,
   )
+  conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+  deleted = models.BooleanField()
+
+
+
+
